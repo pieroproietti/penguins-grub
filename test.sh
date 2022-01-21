9cb2aefb953d65c4d50f2406b6c156da217846a5
Piero Proietti <piero.proietti@gmail.com>
2022-01-21 16:37:15 +0100

    pulizia...

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-TEST=$1/boot/grub
+TEST=$1
 echo "Testing grub ${TEST}"
 echo "per uscire, batti C. quindi EXIT"
 grub-emu --directory=${TEST} --device-map=./device.map --verbose
